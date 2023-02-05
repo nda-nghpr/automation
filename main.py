ddbfc95c7c2e0bbc642b58d458f3e7117b0a2c69
neda <124077118+nda-nghpr@users.noreply.github.com>
2023-02-06 00:08:37 +0330

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-driver= webdriver.Chrome(executable_path=ChromeDriverManager().install())
-driver.get('https://www.google.com/')
